28eda459b01df3d31bb6c4937248583f8a52a3b3
TravisCI <anton.tayanovskyy@gmail.com>
2014-05-10 18:21:10 -0400

    Let us try to deploy via HTTPS

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 
 git config credential.helper "store --file=.git/credentials"
 echo "https://$GH_TOKEN:@github.com" > .git/credentials	
-git remote add live git@github.com:t0yv0/t0yv0.github.io.git
+git remote add live https://github.com/t0yv0/t0yv0.github.io.git
 git config user.name "TravisCI"
 git config user.email "anton.tayanovskyy@gmail.com"
 git add .
