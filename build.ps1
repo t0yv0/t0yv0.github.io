dc0721a27d6a04d850e34515971e252c1c87c7ad
Anton Tayanovskyy (t0yv0) <anton.tayanovskyy@gmail.com>
2015-05-17 00:48:54 -0700

    Reorganizing

--- build.ps1
+++
@@ -1,2 +0,0 @@
-set-alias scribble "C:\Program Files\Racket\scribble.exe"
-scribble ++style content.css --dest-name index content.scrbl
--- clean.ps1
+++
@@ -1,5 +0,0 @@
-rm manual-*.css
-rm manual-*.js
-rm scribble*.css
-rm scribble-*.js
-rm index*.html
