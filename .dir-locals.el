bd62f975abddf2c2f73f4f0fc25c4aa5fd76f6ef
Anton Tayanovskyy <anton.tayanovskyy@gmail.com>
2024-11-02 21:57:16 -0400

    Reset to org

---
+++ .dir-locals.el
@@ -0,0 +1,10 @@
+((nil . ((org-publish-project-alist
+          .
+          (("org"
+            :base-directory "~/code/t0yv0.github.io/"
+            :publishing-function org-html-publish-to-html
+            :publishing-directory "~/code/t0yv0.github.io/"
+            :html-postamble nil
+            :section-numbers nil
+            :with-toc nil
+            :html-head "<link rel=\"stylesheet\" href=\"https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css\" type=\"text/css\"/>"))))))
